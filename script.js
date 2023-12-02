4a6b08731789e570ed076219cf4fd6df2509dfe7
Ricardo <rvillalobosp@gmail.com>
2023-12-02 11:29:54 -0500

    cambio_de_estructura_y_color

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-// Aquí iría la lógica para obtener datos del sensor
-// Por ahora, usaremos datos de ejemplo
 
-function fetchSensorData() {
-    // Simular datos de ejemplo
-    return {
-        temperature: Math.random() * 30,
-        humidity: Math.random() * 100
-    };
+function exportData() {
 }
 
-function updateSensorData() {
-    const data = fetchSensorData();
-    document.getElementById('temperature').textContent = data.temperature.toFixed(2);
-    document.getElementById('humidity').textContent = data.humidity.toFixed(2);
-}
+document.getElementById('export-btn').addEventListener('click', exportData);
 
-setInterval(updateSensorData, 2000); // Actualizar cada 2 segundos
